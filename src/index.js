e1b3604e29e53c7b300755b321d8562d5250910a
Sarhank44 <you@example.com>
2022-07-25 11:54:07 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// ============================[Requirements]========================
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const mongoose = require("mongoose");
+const app = express();
+
+
+app.use(bodyParser.json());
+
+// =============================[ Connect DataBase ]=========================
+mongoose
+  .connect(
+    "mongodb+srv://sarhank44:sarhank8299@sarhancluster.fxjt3wn.mongodb.net/gorup6Database",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("database is connected "))
+  .catch((err) => console.log(err.message));
+
+app.use("/", route);
+
+app.all('/**', (req, res) => {
+  res.status(404).send({ status: false, message: "Either Page Not Found! or You are missing some of the ParaMeters" })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app is running on port " + (process.env.PORT || 3000));
+});
